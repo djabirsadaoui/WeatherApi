3459de16e4716d31b93fc5c8a4f1ba9d9641fb6a
djabirsadaoui <sadaoui.djabir@yahoo.com>
2020-08-11 16:47:52 +0200

    Initial Commit

---
+++ WeatherApi/WeatherApi.h
@@ -0,0 +1,19 @@
+//
+//  WeatherApi.h
+//  WeatherApi
+//
+//  Created by dsadaoui on 11/08/2020.
+//  Copyright © 2020 djabir. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WeatherApi.
+FOUNDATION_EXPORT double WeatherApiVersionNumber;
+
+//! Project version string for WeatherApi.
+FOUNDATION_EXPORT const unsigned char WeatherApiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WeatherApi/PublicHeader.h>
+
+
